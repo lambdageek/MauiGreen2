403c9b15c5b9cb306fadff7bb1d55099b1187104
Aleksey Kliger <alklig@microsoft.com>
2023-06-27 15:47:05 -0400

    Initial Commit

---
+++ MauiGreenTwo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiGreenTwo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiGreenTwo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiGreenTwo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
